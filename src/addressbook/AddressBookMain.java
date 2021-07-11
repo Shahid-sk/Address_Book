e42e00c67cee1fa1dcb8673b42dea518ffa93ae6
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-11 15:16:35 +0530

    created contact_info class

--- src/addressbook/AddressBookMain.java
+++ src/addressbook/AddressBookMain.java
@@ -1,5 +1,75 @@
 package addressbook;
 
+class ContactInfo {
+	private String firstName, lastName, address, city, state, email;
+	private long zip, phonenumber;
+
+	/* generating getters and setters for ContactInfo class */
+	public String getFirstname() {
+		return firstName;
+	}
+
+	public void setFirstname(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastname() {
+		return lastName;
+	}
+
+	public void setLastname(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getcity() {
+		return city;
+	}
+
+	public void setcity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getZip() {
+		return zip;
+	}
+
+	public void setZip(long zip) {
+		this.zip = zip;
+	}
+
+	public long getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(long phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+}
+
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
