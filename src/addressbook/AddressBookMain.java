db7274594684e0e59cf180f658c0e4e1651f852e
Shahid-sk <83329724+Shahid-sk@users.noreply.github.com>
2021-07-12 21:37:37 +0530

    Update AddressBookMain.java

--- src/addressbook/AddressBookMain.java
+++ src/addressbook/AddressBookMain.java
@@ -3,6 +3,83 @@ package addressbook;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class AddContact {
+	private String firstName, lastName, address, city, state, email;
+	private long zip, phonenumber;
+
+	/* generating getters and setters for ContactInfo class */
+	public String getFirstname() {
+		return firstName;
+	}
+
+	public void setFirstname(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastname() {
+		return lastName;
+	}
+
+	public void setLastname(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getZip() {
+		return zip;
+	}
+
+	public void setZip(long zip) {
+		this.zip = zip;
+	}
+
+	public long getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(long phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	/* overriding */
+	public String toString() {
+		return " first name:" + firstName + "\nlast name:" + lastName + "\naddress:" + address + "\ncity:" + city + "\nstate:"
+				+ state + "\nemail:" + email + "\nzipcode:" + zip + "\n phone number: " + phonenumber;
+	}
+
+}
+
 public class AddressBookMain {
 	Scanner sc = new Scanner(System.in); // Scanner class
 	ArrayList<AddContact> arraycontactDetails = new ArrayList<>();
